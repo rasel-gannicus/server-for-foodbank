349c40ae79935d38414432957108a5ed0032d36d
shafik720 <shafikrasel5@gmail.com>
2023-10-21 23:28:52 +0600

    trying to resolve the cors error

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const port = process.env.PORT || 4000;
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials : true
-  };
+app.use((req, res, next) => { res.header({"Access-Control-Allow-Origin": "*"}); next(); })
   
   app.use(cors({
     origin: "http://localhost:5173",
